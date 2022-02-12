83a55b783c701a6db7c1abdc2ed56d377e0a59c4
ekusiadadus <ekusiadadus@gmail.com>
2022-02-12 12:57:57 +0000

    add: login authController

--- app/controllers/authController.go
+++ app/controllers/authController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"go_fiber/app/database"
 	"go_fiber/app/models"
 
 	"github.com/gofiber/fiber/v2"
@@ -20,5 +21,36 @@ func Register(c *fiber.Ctx) error{
 		Email: data["email"],
 		Password: password,
 	}
+
+	database.DB.Create(&user)
+	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error{
+	
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "user not found",
+		})
+	}
+
+	if err:= bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil{
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Incorrect password",
+		})
+	}
+
 	return c.JSON(user)
 }
