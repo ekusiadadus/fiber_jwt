5ab507599370f6bdefab0d1edf4a73a11345e619
ekusiadadus <ekusiadadus@gmail.com>
2022-02-12 14:23:42 +0000

    add: middleware

--- app/main.go
+++ app/main.go
@@ -5,11 +5,15 @@ import (
 	"go_fiber/app/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	database.Connect()
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 	routes.Setup(app)
 	app.Listen(":3000")
 }
