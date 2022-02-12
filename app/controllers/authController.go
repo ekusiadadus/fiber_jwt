cc2bdd7eb1824c4c2ae9553f06c672051f622465
ekusiadadus <ekusiadadus@gmail.com>
2022-02-12 12:46:26 +0000

    add: new User Struct

--- app/controllers/authController.go
+++ app/controllers/authController.go
@@ -1,6 +1,11 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"go_fiber/app/models"
+
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func Register(c *fiber.Ctx) error{
 	var data map[string]string
@@ -9,5 +14,11 @@ func Register(c *fiber.Ctx) error{
 		return err
 	}
 	
-	return c.JSON(data)
+	password, _  := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+	user := models.User{
+		Name: data["name"],
+		Email: data["email"],
+		Password: password,
+	}
+	return c.JSON(user)
 }
