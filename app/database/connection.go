cf6d79f297968dea500b5fe922e57025ed3fb026
ekusiadadus <ekusiadadus@gmail.com>
2022-02-12 12:58:03 +0000

    add: get user

--- app/database/connection.go
+++ app/database/connection.go
@@ -7,10 +7,13 @@ import (
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func Connect() {
 	connection, err := gorm.Open(mysql.Open("root:root@tcp(localhost:3306)/go_jwt"), &gorm.Config{})
 	if err != nil {
 		panic("Could not connect to the database")
 	}
+	DB = connection
 	connection.AutoMigrate(&models.User{})
 }
