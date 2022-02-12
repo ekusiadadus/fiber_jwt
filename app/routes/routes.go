839d4059d65100cfd65136b17191917e5edebf7a
ekusiadadus <ekusiadadus@gmail.com>
2022-02-12 10:44:53 +0000

    add: authmethods

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -1,9 +1,11 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"go_fiber/app/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Setup(app *fiber.App) {
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+	app.Post("/api/register", controllers.Register)
 }
