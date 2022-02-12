16759d37646714454318387ebafe4d710bc13669
ekusiadadus <ekusiadadus@gmail.com>
2022-02-12 18:06:04 +0000

    fix: onSubmit to onClick

--- frontend/src/pages/LogInPage.tsx
+++ frontend/src/pages/LogInPage.tsx
@@ -1,15 +1,37 @@
+import { SyntheticEvent, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 
 const LogInPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const response = await fetch("http://127.0.0.1:8000/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    console.log(await response.json());
+  };
   return (
     <Container className="mt-5 mb-5" style={{ backgroundColor: "#fff" }}>
       <main className="form-signin">
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
@@ -17,12 +39,17 @@ const LogInPage = () => {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
           </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="submit" onClick={submit}>
             Submit
           </Button>
         </Form>
--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -12,7 +12,6 @@ const SignUpPage = () => {
   const [redirect, setRedirect] = useState(false);
 
   const submit = async (e: SyntheticEvent) => {
-    e.preventDefault();
     await fetch("http://127.0.0.1:8000/api/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
