2b8f26cf4e6deab3f1c1789d4628d3453ad7fe6e
ekusiadadus <ekusiadadus@gmail.com>
2022-02-12 12:46:43 +0000

    add: mysql autoMigration

--- app/database/connection.go
+++ app/database/connection.go
@@ -1,13 +1,16 @@
 package database
 
 import (
+	"go_fiber/app/models"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Connect() {
-	_, err := gorm.Open(mysql.Open("root:root@tcp(localhost:3306)/go_jwt"), &gorm.Config{})
+	connection, err := gorm.Open(mysql.Open("root:root@tcp(localhost:3306)/go_jwt"), &gorm.Config{})
 	if err != nil {
 		panic("Could not connect to the database")
 	}
+	connection.AutoMigrate(&models.User{})
 }
