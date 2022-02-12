de0bd69fc573f876f552f90e5cffdefe2e827361
ekusiadadus <ekusiadadus@gmail.com>
2022-02-12 08:23:21 +0000

    add: add main.go connect to mysql

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	_, err := gorm.Open(mysql.Open("test:test@127.0.0.1/go_jwt"), &gorm.Config{})
+	if err != nil {
+		panic("Could not connect to the database")
+	}
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(":3000")
+}
